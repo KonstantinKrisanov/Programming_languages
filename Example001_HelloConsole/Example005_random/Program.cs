4a202a978bbc5c4a9217922568a99de941e5c514
Konstantin <kostya-krisanov@mail.ru>
2022-08-30 21:41:45 +0300

    Ex005

---
+++ Example001_HelloConsole/Example005_random/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = new Random().Next(1,10); //в скобках указывается диапозон 1 2 3 4 .... 9
+Console.WriteLine(a);
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
+int c = a + b;
+Console.Write("Ответ" );
+Console.WriteLine(c);
