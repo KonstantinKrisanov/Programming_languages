8972fd14dac082c0e0b1ea9e9be903659dcbcb65
Konstantin <kostya-krisanov@mail.ru>
2022-08-30 18:34:40 +0300

    Ex003

---
+++ Example001_HelloConsole/Example003_sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Сложение двух целых чисел
+Console.WriteLine("Введите первое число  ");
+int a = Consol.ReadLine();
+Console.WriteLine("Введите второе число  ");
+int b = Consol.ReadLine()
+int c = a + b;
+Consol.WriteLine(c);
