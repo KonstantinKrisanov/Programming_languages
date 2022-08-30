26b16d714dd8ff9ba101e5a2753bd52294587862
Konstantin <kostya-krisanov@mail.ru>
2022-08-30 22:08:19 +0300

    Ex005

---
+++ Example001_HelloConsole/Examples006_if_elce/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input you name  ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+    { 
+        Console.WriteLine("ОГО ЭТО ЖЕ МАША!!!");
+    }
+else
+{
+        Console.Write("Привет  ") ;
+        Console.WriteLine(username);
+}
