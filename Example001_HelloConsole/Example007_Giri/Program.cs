46da12f296865b029fdc5bb199076e870e5a0f2a
Konstantin <kostya-krisanov@mail.ru>
2022-08-30 22:21:46 +0300

    Ex007

---
+++ Example001_HelloConsole/Example007_Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 9;
+int c = 4;
+int d = 6;
+int e = 7;
+
+int max = 0;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Вес самой большой гири =  ");
+Console.WriteLine(max);
