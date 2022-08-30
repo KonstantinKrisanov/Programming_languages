d72469906294821a448aa648fbb2bba4a7c700e1
Konstantin <kostya-krisanov@mail.ru>
2022-08-30 18:12:06 +0300

    Ex002

---
+++ Example001_HelloConsole/Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter your name  ");
+string username = Console.ReadLine();  //то что с лева присваивается то что справа
+Console.Write("Hello  ");
+Console.WriteLine(username);  
